e7e504efe281d685a5a1fd37a7595e54a168d5af
satwika putri <satwika.zahrani@gmail.com>
2026-06-21 21:09:43 +0700

    fix: force mysqli to use 127.0.0.1 instead of localhost

--- config/database.php
+++ config/database.php
@@ -1,39 +1,17 @@
 <?php
-// Mengambil kredensial private network Railway secara dinamis
-define('DB_HOST', getenv('MYSQLHOST') ?: '127.0.0.1');
-define('DB_USER', getenv('MYSQLUSER') ?: 'root');
-define('DB_PASS', getenv('MYSQLPASSWORD') ?: '');
-define('DB_NAME', getenv('MYSQLDATABASE') ?: 'railway');
-define('DB_PORT', getenv('MYSQLPORT') ?: '3306');
-
-function getDB() {
-    // Pastikan DB_PORT di-cast menjadi integer agar fungsi mysqli tidak bingung
-    $port = (int)DB_PORT;
-    
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, $port);
-    
-    if ($conn->connect_error) {
-        die("Koneksi gagal: " . $conn->connect_error);
-    }
-    
-    $conn->set_charset("utf8mb4");
-    return $conn;
-}
 
 echo "<pre>";
 
-echo "MYSQLHOST: ";
+echo "MYSQLHOST = ";
 var_dump(getenv('MYSQLHOST'));
 
-echo "MYSQLUSER: ";
+echo "\nMYSQLUSER = ";
 var_dump(getenv('MYSQLUSER'));
 
-echo "MYSQLDATABASE: ";
+echo "\nMYSQLDATABASE = ";
 var_dump(getenv('MYSQLDATABASE'));
 
-echo "MYSQLPORT: ";
+echo "\nMYSQLPORT = ";
 var_dump(getenv('MYSQLPORT'));
 
 exit;
-
-?>
